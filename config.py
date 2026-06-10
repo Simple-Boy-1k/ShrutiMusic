5ad1aba45dbf85c43fb5c731a2837b0c88540bf4
𝘚𝘪𝘮𝘱𝘭𝘦_𝘉𝘰𝘺 <sksahnawaj89@gmail.com>
2026-06-10 21:26:41 +0530

    Update image URLs in config.py

--- config.py
+++ config.py
@@ -48,16 +48,16 @@ AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 
 START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/rm08b5.jpg")
 PING_IMG_URL = "https://files.catbox.moe/z1h6ow.jpg"
-PLAYLIST_IMG_URL = "https://files.catbox.moe/lrwbj6.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/lrwbj6.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/eehxb4.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/z1h6ow.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/z1h6ow.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/z1h6ow.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/z1h6ow.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/z1h6ow.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/z1h6ow.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/z1h6ow.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/z1h6ow.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/z1h6ow.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/z1h6ow.jpg"
 
 BANNED_USERS = filters.user()
 adminlist = {}
